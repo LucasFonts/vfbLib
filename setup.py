c25be12f2542b43466b4e73ff412196f4b615aa9
Jens Kutilek <webmail@kutilek.de>
2022-10-23 18:43:22 +0200

    Add boilerplate stuff

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+# from mypyc.build import mypycify
+
+
+if __name__ == "__main__":
+    setup(
+        # ext_modules=mypycify([
+        #     "Lib/vfbLib/tools/helpers.py",
+        # ]),
+    )
