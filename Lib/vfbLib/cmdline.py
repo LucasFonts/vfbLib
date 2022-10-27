e326632bb78cfa95219e5d131640a102685f86f5
Jens Kutilek <webmail@kutilek.de>
2022-10-27 15:15:11 +0200

    Begin adding vfb2ufo/vfb3ufo

--- Lib/vfbLib/cmdline.py
+++ Lib/vfbLib/cmdline.py
@@ -22,3 +22,67 @@ def vfb2json():
         path = Path(arg)
         reader = read_vfb(path)
         write_json(reader, path)
+
+
+def vfb2ufo():
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(
+        description=(
+            "VFB3UFO Converter\n"
+            "Copyright (c) 2022 by LucasFonts\n"
+            "Build 2022-10-27"
+        )
+    )
+    parser.add_argument(
+        "-p",
+        "--path",
+        type=str,
+        nargs=1,
+        help="output folder",
+    )
+    parser.add_argument(
+        "-fo",
+        "--force-overwrite",
+        action="store_true",
+        default=False,
+        help="force overwrite",
+    )
+    parser.add_argument(
+        "-ttx",
+        "--ttx",
+        action="store_true",
+        default=False,
+        help="convert binary OpenType Layout data using TTX-like format",
+    )
+    parser.add_argument(
+        "-64",
+        "--base64",
+        action="store_true",
+        default=False,
+        help="write GLIF lib 'data' section using base64 (recommended)",
+    )
+    parser.add_argument(
+        "-z",
+        "--zip",
+        action="store_true",
+        default=False,
+        help="write UFOZ (compressed UFO)",
+    )
+    parser.add_argument(
+        "inputpath",
+        type=str,
+        nargs=1,
+        help="input file path (.vfb)",
+    )
+    parser.add_argument(
+        "outputpath",
+        type=str,
+        nargs="?",
+        help="output file path (.ufo[z])",
+    )
+    args = parser.parse_args()
+    if args:
+        reader = read_vfb(Path(args.inputpath))
+    else:
+        parser.print_help()
