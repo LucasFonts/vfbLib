e5be7f119c60650946fc5e500b41ddf139c5baa7
Jens Kutilek <webmail@kutilek.de>
2022-10-27 15:13:38 +0200

    Add typing annotation

--- Lib/vfbLib/cmdline.py
+++ Lib/vfbLib/cmdline.py
@@ -5,19 +5,20 @@ from sys import argv
 from vfbLib import VFBReader
 
 
-def read_vfb(vfb_path):
+def read_vfb(vfb_path: Path) -> VFBReader:
     reader = VFBReader(vfb_path)
     reader.read()
     return reader
 
 
-def write_json(reader, vfb_path):
-    out_path = Path(vfb_path).with_suffix(".json")
+def write_json(reader: VFBReader, vfb_path: Path) -> None:
+    out_path = vfb_path.with_suffix(".json")
     with codecs.open(str(out_path), "wb", "utf-8") as f:
         json.dump(reader.data, f, ensure_ascii=False, indent=4)
 
 
 def vfb2json():
     for arg in argv[1:]:
-        reader = read_vfb(arg)
-        write_json(reader, arg)
+        path = Path(arg)
+        reader = read_vfb(path)
+        write_json(reader, path)
