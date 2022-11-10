b9cfe392811f8059e27bf71502898daa977c69af
Jens Kutilek <webmail@kutilek.de>
2022-11-10 16:29:33 +0100

    Remove list from scaleX, scaleY

--- Lib/vfbLib/parsers/glyph.py
+++ Lib/vfbLib/parsers/glyph.py
@@ -212,8 +212,8 @@ class GlyphParser(BaseParser):
             for _ in range(num_masters):
                 x = read_encoded_value(stream)
                 y = read_encoded_value(stream)
-                scaleX = unpack("d", stream.read(8))
-                scaleY = unpack("d", stream.read(8))
+                scaleX = unpack("d", stream.read(8))[0]
+                scaleY = unpack("d", stream.read(8))[0]
                 c["offsetX"].append(x)
                 c["offsetY"].append(y)
                 c["scaleX"].append(scaleX)
