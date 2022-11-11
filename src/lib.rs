63b9880bf6860e6347ef8cd52edcd5f7df3a3879
Jens Kutilek <webmail@kutilek.de>
2022-11-11 19:04:21 +0100

    Slow progress

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,33 @@ use std::fs::File;
 
 // internal functions
 
-fn read_encoded_value<R>(r: &BufReader<R>) -> i32 {
-    return 0;
+fn read_encoded_value<R>(r: &mut BufReader<R>) -> i32 where R: std::io::Read {
+    let mut value = [0; 1];
+    r.read_exact(&mut value[..]).expect("ValueError");
+    let val = value[0];
+    if val < 0x20 {
+        return 0;
+    } else if val < 0xF7 {
+        return (val - 0x8B).into();
+    } else if val <= 0xFA {
+        let mut value2 = [0; 1];
+        r.read_exact(&mut value2[..]).expect("ValueError");
+        let val2 = value2[0];
+        return (val - 0x8B + (val - 0xF7) * 0xFF + val2).into();
+    } else if val <= 0xFE {
+        let mut value2 = [0; 1];
+        r.read_exact(&mut value2[..]).expect("ValueError");
+        let val2 = value2[0];
+        return (0x8F - val - (val - 0xFB) * 0xFF - val2).into();
+    } else if val == 0xFF {
+        let mut value2 = [0; 4];
+        r.read_exact(&mut value2[..]).expect("ValueError");
+        // FIXME: Add values
+        let val2 = value2[0];
+        return val2.into();
+    } else {
+        return 0;
+    }
 }
 
 // struct VfbHeader {
@@ -40,10 +65,10 @@ fn read_vfb(path: &str) -> PyResult<()> {
     let mut r = BufReader::new(file);
 
     let mut header0 = [0; 1];
-    let mut filetype = [0; 5];
+    let mut filetyp = [0; 5];
     let mut header1 = [0; 2];
     let mut header2 = [0; 2];
-    let mut reserved = [0; 34];
+    let mut reserve = [0; 34];
     let mut header3 = [0; 2];
     let mut header4 = [0; 2];
     let mut header5 = [0; 2];
@@ -52,10 +77,10 @@ fn read_vfb(path: &str) -> PyResult<()> {
     let mut header8 = [0; 2];
 
     r.read_exact(&mut header0[..])?;
-    r.read_exact(&mut filetype[..])?;
+    r.read_exact(&mut filetyp[..])?;
     r.read_exact(&mut header1[..])?;
     r.read_exact(&mut header2[..])?;
-    r.read_exact(&mut reserved[..])?;
+    r.read_exact(&mut reserve[..])?;
     r.read_exact(&mut header3[..])?;
     r.read_exact(&mut header4[..])?;
     r.read_exact(&mut header5[..])?;
@@ -63,15 +88,23 @@ fn read_vfb(path: &str) -> PyResult<()> {
     r.read_exact(&mut header7[..])?;
     r.read_exact(&mut header8[..])?;
 
-    println!("{:#?}", filetype);
+    // println!("{:#?}", filetype);
 
     // Read 3 key/value pairs (?)
     let mut _k = [0; 1];
     let mut _v: i32 = -1;
     for _i in 9..=11 {
         r.read_exact(&mut _k[..])?;
-        _v = read_encoded_value(&r);
+        _v = read_encoded_value(&mut r);
+        println!("{:#?}", _k[0]);
+        println!("{:#?}", _v);
     }
+
+    let mut header9 = [0; 2];
+    let mut headera = [0; 2];
+
+    r.read_exact(&mut header9[..])?;
+    r.read_exact(&mut headera[..])?;
 
     // let mut obj = ...
     let _obj = VfbObject {
