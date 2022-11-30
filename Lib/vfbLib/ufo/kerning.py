8dbe88de1c22b7b93e525e77f802e7c6018fa204
Jens Kutilek <webmail@kutilek.de>
2022-11-30 15:54:00 +0100

    Support kerning groups

--- Lib/vfbLib/ufo/kerning.py
+++ Lib/vfbLib/ufo/kerning.py
@@ -20,10 +20,31 @@ class UfoKerning:
         self.master_index = master_index
         self.master_kerning: UfoMasterKerning = {}
 
-    def extract_master_kerning(self, master_index) -> UfoMasterKerning:
-        # FIXME: Must look up class kerning references
+    def extract_master_kerning(self, master_index) -> None:
+        """
+        Extract the kerning value for master_index. Also solves group kerning
+        references.
+        """
         self.master_kerning = kerning = {}
         for pair, values in self.mm_kerning.items():
             L, Rid = pair
-            kerning[L, self.glyphOrder[int(Rid)]] = values[master_index]
-        return kerning
+            # Make right GID into glyph name
+            R = self.glyphOrder[int(Rid)]
+
+            # Is the left glyph a keyglyph? It is so if there's a kerning group
+            # named after it. In that case, use the group name instead of the
+            # glyph name.
+            left_group = f"public.kern1.{L}"
+            if left_group in self.groups:
+                left = left_group
+            else:
+                left = L
+
+            # Is the right glyph a keyglyph?
+            right_group = f"public.kern2.{R}"
+            if right_group in self.groups:
+                right = right_group
+            else:
+                right = R
+
+            kerning[left, right] = values[master_index]
