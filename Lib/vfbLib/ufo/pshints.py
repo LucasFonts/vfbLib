f061f0a682537eedce08884e9441a2b87616eea9
Jens Kutilek <webmail@kutilek.de>
2022-12-01 17:21:14 +0100

    Unindent by one level

--- Lib/vfbLib/ufo/pshints.py
+++ Lib/vfbLib/ufo/pshints.py
@@ -39,42 +39,42 @@ def build_ps_glyph_hints(
 def get_master_hints(
     mmglyph: VfbToUfoGlyph, glyph: Glyph, master_index=0
 ) -> List[Tuple[str, int, int]]:
-        hints = []
+    hints = []
 
-        # Hints
-        for d in "hv":
-            dh = mmglyph.mm_hints[d]
-            for mm_hints in dh:
-                hint = mm_hints[master_index]
-                hints.append((f"{d}stem", hint["pos"], hint["width"]))
+    # Hints
+    for d in "hv":
+        dh = mmglyph.mm_hints[d]
+        for mm_hints in dh:
+            hint = mm_hints[master_index]
+            hints.append((f"{d}stem", hint["pos"], hint["width"]))
 
-        # Links
-        if not mmglyph.links:
-            return hints
-
-        # Convert links to hints
-        for i, d in enumerate("xy"):
-            dl = mmglyph.links[d]
-            for link in dl:
-                isrc, itgt = link  # indices of source and target node
-                src = mmglyph.mm_nodes[isrc]
-                src_pos = src["points"][master_index][0][i]
-                pos = src_pos
-                if itgt == -2:
-                    # Bottom ghost
-                    width = -21
-                elif itgt == -1:
-                    # Top ghost
-                    width = -20
-                else:
-                    tgt = mmglyph.mm_nodes[itgt]
-                    tgt_pos = tgt["points"][master_index][0][i]
-                    width = abs(tgt_pos - src_pos)
-                    pos = min(src_pos, tgt_pos)
-                stem_dir = "v" if d == "x" else "h"
-                hints.append((f"{stem_dir}stem", pos, width))
-
+    # Links
+    if not mmglyph.links:
         return hints
+
+    # Convert links to hints
+    for i, d in enumerate("xy"):
+        dl = mmglyph.links[d]
+        for link in dl:
+            isrc, itgt = link  # indices of source and target node
+            src = mmglyph.mm_nodes[isrc]
+            src_pos = src["points"][master_index][0][i]
+            pos = src_pos
+            if itgt == -2:
+                # Bottom ghost
+                width = -21
+            elif itgt == -1:
+                # Top ghost
+                width = -20
+            else:
+                tgt = mmglyph.mm_nodes[itgt]
+                tgt_pos = tgt["points"][master_index][0][i]
+                width = abs(tgt_pos - src_pos)
+                pos = min(src_pos, tgt_pos)
+            stem_dir = "v" if d == "x" else "h"
+            hints.append((f"{stem_dir}stem", pos, width))
+
+    return hints
 
 
 def update_adobe_hinting(data) -> UfoHintingV2:
