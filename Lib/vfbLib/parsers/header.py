a4d71944e43f593048ff484e6f346a14eef97699
Jens Kutilek <webmail@kutilek.de>
2022-12-07 15:51:47 +0100

    Improve typing in header parser

--- Lib/vfbLib/parsers/header.py
+++ Lib/vfbLib/parsers/header.py
@@ -2,24 +2,21 @@ from __future__ import annotations
 
 from io import BufferedReader
 from typing import Any, Dict
-from vfbLib.parsers import BaseParser, read_encoded_value
+from vfbLib.parsers import read_encoded_value, uint8, uint16
 
 
-class VfbHeaderParser(BaseParser):
+class VfbHeaderParser:
+    stream: BufferedReader | None = None
+
     @classmethod
-    def parse(
-        cls,
-        stream: BufferedReader,
-        size: int = 0,
-        master_count: int | None = None,
-    ):
+    def parse(cls, stream: BufferedReader):
         cls.stream = stream
         header: Dict[str, Any] = {}
         header["header0"] = cls.read_uint8()
-        header["filetype"] = stream.read(5).decode("cp1252")
+        header["filetype"] = cls.stream.read(5).decode("cp1252")
         header["header1"] = cls.read_uint16()
         header["header2"] = cls.read_uint16()
-        header["reserved"] = str(stream.read(34))
+        header["reserved"] = str(cls.stream.read(34))
         header["header3"] = cls.read_uint16()
         header["header4"] = cls.read_uint16()
         header["header5"] = cls.read_uint16()
@@ -36,3 +33,17 @@ class VfbHeaderParser(BaseParser):
         header["header14"] = cls.read_uint8()
 
         return header
+
+    @classmethod
+    def read_uint8(cls) -> int:
+        assert cls.stream is not None
+        return int.from_bytes(
+            cls.stream.read(uint8), byteorder="little", signed=False
+        )
+
+    @classmethod
+    def read_uint16(cls) -> int:
+        assert cls.stream is not None
+        return int.from_bytes(
+            cls.stream.read(uint16), byteorder="little", signed=False
+        )
