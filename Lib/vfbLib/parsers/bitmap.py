bc5505bd6c593efaa557bb7b8249c7765b90c290
Jens Kutilek <webmail@kutilek.de>
2022-12-13 17:14:14 +0100

    WIP code to graphically view bitmaps

--- Lib/vfbLib/parsers/bitmap.py
+++ Lib/vfbLib/parsers/bitmap.py
@@ -2,11 +2,44 @@ from __future__ import annotations
 
 import logging
 
+from fontTools.misc.textTools import hexStr, num2binary
+# from math import log2
+# from struct import unpack
 from typing import Any, Dict, List
 from vfbLib.parsers import BaseParser, read_encoded_value
 
 
 logger = logging.getLogger(__name__)
+
+
+def pprint_bitmap(bitmap, datalen) -> List[str]:
+    # print(bitmap)
+    mask = bitmap["mask"]
+    data = bitmap["data"]
+    b: List[str] = []
+    for v in data:
+        b.append(num2binary(v, bits=8).replace("0", "  ").replace("1", "██"))
+    # i += 1
+    # cols = bitmap["size"][0]
+    # col_bytes = cols // 8
+    # print("col_bytes", col_bytes)
+    # rest = cols % 8
+    # if rest > 0:
+    #     col_bytes += 1
+    # print(f"Cols: {cols}, bytes per line: {col_bytes}")
+    # line = ""
+    # for j in range(col_bytes):
+    #     print("byte", j)
+    #     if i < datalen:
+    #         b = cls.read_uint8()
+    #         i += 1
+    #         pixels = num2binary(b, bits=8).replace("0", "  ").replace("1", "██")
+    #         line += pixels
+    #     else:
+    #         print("Hit end of data")
+    # print(f"|{line}|")
+    # data.append(line)
+    return b
 
 
 class BaseBitmapParser(BaseParser):
@@ -20,6 +53,7 @@ class BaseBitmapParser(BaseParser):
                 b = cls.read_uint8()
                 data.append(b)
             bitmap["data"] = data
+            bitmap["dat2"] = pprint_bitmap(bitmap, datalen)
         return bitmap
 
 
