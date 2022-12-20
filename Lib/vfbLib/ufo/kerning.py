b287f604d7d0c1d4e61a2e5e9ea0a289ba9ab284
Jens Kutilek <webmail@kutilek.de>
2022-12-20 13:41:18 +0100

    Export kerning exceptions with value 0

--- Lib/vfbLib/ufo/kerning.py
+++ Lib/vfbLib/ufo/kerning.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import logging
 
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING, Dict, List, Tuple
 
 if TYPE_CHECKING:
     from vfbLib.ufo.typing import UfoGroups, UfoMasterKerning, UfoMMKerning
@@ -17,20 +17,65 @@ class UfoKerning:
         glyphOrder: List[str],
         groups: UfoGroups,
         mm_kerning: UfoMMKerning,
-        master_index: int = 0,
     ):
         self.glyphOrder = glyphOrder
         self.groups = groups
         self.mm_kerning = mm_kerning
-        self.master_index = master_index
         self.master_kerning: UfoMasterKerning = {}
+        self._build_glyph_to_groups_mapping()
+        self._make_name_based_kerning()
 
-    def extract_master_kerning(self, master_index) -> None:
+    def _build_glyph_to_groups_mapping(self) -> None:
         """
-        Extract the kerning value for master_index. Also solves group kerning
+        Glyph name to kerning groups
+        """
+        self.glyph_group_1: Dict[str, str] = {}
+        self.glyph_group_2: Dict[str, str] = {}
+        for group in self.groups.items():
+            name, glyphs = group
+            if name.startswith("public.kern1"):
+                for g in glyphs:
+                    if g in self.glyph_group_1:
+                        logger.error(
+                            f"Multiple kerning groups (kern1) for glyph {g}: "
+                            f"{self.glyph_group_1[g]}, {name}. New group ignored."
+                        )
+                        continue
+                    self.glyph_group_1[g] = name
+            elif name.startswith("public.kern2"):
+                for g in glyphs:
+                    if g in self.glyph_group_2:
+                        logger.error(
+                            f"Multiple kerning groups (kern2) for glyph {g}: "
+                            f"{self.glyph_group_2[g]}, {name}. New group ignored."
+                        )
+                        continue
+                    self.glyph_group_2[g] = name
+
+    def _is_exception(self, L: str, R: str):
+        L_group = f"public.kern1.{L}"
+        if L_group in self.groups:
+            left_is_key = True
+        else:
+            left_is_key = L not in self.glyph_group_1
+
+        right_group = f"public.kern2.{R}"
+        if right_group in self.groups:
+            right_is_key = True
+        else:
+            right_is_key = R not in self.glyph_group_2
+
+        if left_is_key and right_is_key:
+            return False
+
+        return True
+
+    def _make_name_based_kerning(self) -> None:
+        """
+        Convert the glyph indices to glyph names. Also solves group kerning
         references.
         """
-        self.master_kerning = {}
+        self.mm_kerning_names: Dict[Tuple[str, str], List[int]] = {}
         for pair, values in self.mm_kerning.items():
             L, Rid = pair
             # Make right GID into glyph name
@@ -51,7 +96,14 @@ class UfoKerning:
                 right = right_group
             else:
                 right = R
+            self.mm_kerning_names[left, right] = values
 
+    def extract_master_kerning(self, master_index) -> None:
+        """
+        Extract the kerning value for master_index.
+        """
+        self.master_kerning = {}
+        for pair, values in self.mm_kerning_names.items():
             value = values[master_index]
-            if value != 0:
-                self.master_kerning[left, right] = value
+            if value != 0 or self._is_exception(*pair):
+                self.master_kerning[*pair] = value
---
+++ Tests/ufo/kerning_test.py
@@ -0,0 +1,63 @@
+from unittest import TestCase
+
+from vfbLib.ufo.kerning import UfoKerning
+
+glyph_order = ["A", "Adieresis", "E", "F", "T", "Tbar", "Y"]
+
+groups = {
+    "public.kern1.A": ["A", "Adieresis"],
+    "public.kern2.A": ["A", "Adieresis"],
+    "public.kern1.T": ["T", "Y"],
+    "public.kern2.T": ["T", "Y", "Tbar"],
+    "public.kern1.F": ["F", "Tbar"],
+    "public.kern2.E": ["E", "F"],
+}
+
+mmk = {
+    
+}
+
+
+kerning = UfoKerning(glyphOrder=glyph_order, groups=groups, mm_kerning=mmk)
+kerning.mm_kerning_names = {
+    ("public.kern1.A", "public.kern2.T"): [-100, -80],
+    ("public.kern1.T", "public.kern2.A"): [-100, -80],
+    ("Adieresis", "public.kern2.T"): [-80, -60],
+    ("public.kern1.T", "Adieresis"): [-80, -60],
+    ("public.kern1.F",  "public.kern2.A"): [-70, -65],
+    ("Tbar",  "Adieresis"): [-60, -55],
+    ("P", "A"): [-90, -85],
+    ("P", "J"): [-90, -85],
+}
+
+
+class UfoKerningTest(TestCase):
+    def test_exception_keypair(self):
+        assert not kerning._is_exception("A", "T")
+    
+    def test_exception_key_member(self):
+        assert kerning._is_exception("T", "Adieresis")
+    
+    def test_exception_member_key(self):
+        assert kerning._is_exception("Adieresis", "T")
+    
+    def test_exception_member_member(self):
+        assert kerning._is_exception("Tbar", "Adieresis")
+    
+    def test_exception_nonmember_key(self):
+        assert not kerning._is_exception("P", "A")
+    
+    def test_exception_key_nonmember(self):
+        assert not kerning._is_exception("T", "J")
+    
+    def test_exception_nonmember_member(self):
+        assert kerning._is_exception("Ydieresis", "Adieresis")
+    
+    def test_exception_member_nonmember(self):
+        assert kerning._is_exception("Adieresis", "Ydieresis")
+    
+    def test_exception_nonmember_nonmember(self):
+        assert not kerning._is_exception("P", "J")
+    
+    def test_exception_unkerned(self):
+        assert not kerning._is_exception("X", "hyphen")
