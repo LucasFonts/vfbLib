09b78da29d81cfadf3e7ae9981e63c7402343a09
Jens Kutilek <webmail@kutilek.de>
2023-05-05 10:30:39 +0200

    Implement draw and drawPoints for glyph

--- Lib/vfbLib/vfb/glyph.py
+++ Lib/vfbLib/vfb/glyph.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 
 import logging
 
-from typing import TYPE_CHECKING
+from fontTools.pens.pointPen import PointToSegmentPen
+from typing import TYPE_CHECKING, Type
 
 if TYPE_CHECKING:
+    from fontTools.pens.pointPen import AbstractPointPen
     from vfbLib.vfb.entry import VfbEntry
 
 logger = logging.getLogger(__name__)
@@ -16,6 +18,9 @@ class VfbGlyph:
         self._target_master = 0
 
     def decompile(self) -> str:
+        """
+        Decompile the Glyph entry and return the glyph name.
+        """
         self.entry.decompile()
         if self.entry.decompiled is None:
             raise ValueError
@@ -34,25 +39,58 @@ class VfbGlyph:
     def target_master(self, value: int) -> None:
         self._target_master = value
 
-    def draw(self, pen, glyph_set=None) -> None:
+    def draw(self, pen) -> None:
         """
         Draw the VFB glyph onto a segment pen.
         """
-        raise NotImplementedError
+        sp = PointToSegmentPen(pen, outputImpliedClosingLine=False)
+        self.drawPoints(sp)
 
-    def drawPoints(self, pen, glyph_set=None) -> None:
+    def drawPoints(self, pen: AbstractPointPen) -> None:
         """
         Draw the VFB glyph onto a point pen.
         """
-        raise NotImplementedError
+        if self.entry.decompiled is None:
+            raise ValueError
 
-    def getPen(self):
+        in_path = False
+        in_qcurve = False
+        if "nodes" in self.entry.decompiled:
+            for node in self.entry.decompiled["nodes"]:
+                segment_type = node["type"]
+                if segment_type == "move":
+                    if in_path:
+                        pen.endPath()
+                    pen.beginPath()
+                    in_path = True
+                    # FIXME: Support open paths by checking the flags, and issuing "move"
+                    pen.addPoint(node["points"][self._target_master][0], "line")
+                elif segment_type == "line":
+                    if in_qcurve:
+                        st = "qcurve"
+                        in_qcurve = False
+                    else:
+                        st = segment_type
+                    pen.addPoint(node["points"][self._target_master][0], st)
+                elif segment_type == "curve":
+                    # Reorder the curve points
+                    p1, c0, c1 = node["points"][self._target_master]
+                    pen.addPoint(c0)
+                    pen.addPoint(c1)
+                    pen.addPoint(p1, "curve")
+                elif segment_type == "qcurve":
+                    pen.addPoint(node["points"][self._target_master][0])
+                    in_qcurve = True
+            if in_path:
+                pen.endPath()
+
+    def getPen(self) -> None:
         """
         Get a segment pen to draw into the VFB glyph.
         """
         raise NotImplementedError
 
-    def getPointPen(self):
+    def getPointPen(self) -> None:
         """
         Get a point pen to draw into the VFB glyph.
         """
