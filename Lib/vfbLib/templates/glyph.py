cc8b9cbc59ba5ef3c36a2e68aa7476636a2c7f28
Jens Kutilek <webmail@kutilek.de>
2023-07-12 19:35:48 +0200

    Add template to build an empty JSON glyph

---
+++ Lib/vfbLib/templates/glyph.py
@@ -0,0 +1,11 @@
+from vfbLib import GLYPH_CONSTANT
+
+def get_empty_glyph(num_masters: int):
+    return {
+        "constants": GLYPH_CONSTANT,
+        "metrics": [(0, 0) for _ in range(num_masters)],
+        "name": "",
+        "nodes": [],
+        "num_masters": num_masters,
+        "num_node_values": 0,
+    }
