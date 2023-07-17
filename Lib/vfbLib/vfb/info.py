0e80545cd793519040f3336da447ff4a0d7995d0
Jens Kutilek <webmail@kutilek.de>
2023-07-17 17:53:02 +0200

    Minor

--- Lib/vfbLib/vfb/info.py
+++ Lib/vfbLib/vfb/info.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from functools import cached_property
 from typing import TYPE_CHECKING
-# from vfbLib.ufo.info import VfbToUfoInfo
+
 if TYPE_CHECKING:
     from vfbLib.vfb.vfb import Vfb
 
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
 class VfbInfo:
     def __init__(self, vfb: Vfb) -> None:
         self.parent = vfb
-    
+
     @cached_property
     def unitsPerEm(self) -> int:
         for entry in self.parent.entries:
