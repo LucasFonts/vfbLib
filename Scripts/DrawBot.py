c92702c2b965ad2886cd2b71dd785505e773d60f
Jens Kutilek <webmail@kutilek.de>
2023-07-20 16:39:00 +0200

    Remove unneeded command

--- Scripts/DrawBot.py
+++ Scripts/DrawBot.py
@@ -6,7 +6,6 @@ size(1000, 1000)
 vfb_path = Path(__file__).parent.parent / "Tests" / "Data" / "ComicJensPro-Regular.vfb"
 
 vfb = Vfb(vfb_path)
-vfb.read()
 g = vfb["Adieresis"]
 bez = BezierPath()
 g.drawPoints(bez)
