c702143e45c14764323cd88dd84e5473f12cb522
Jens Kutilek <webmail@kutilek.de>
2023-07-21 11:34:18 +0200

    Also in header parser

--- Lib/vfbLib/parsers/header.py
+++ Lib/vfbLib/parsers/header.py
@@ -13,45 +13,44 @@ logger = logging.getLogger(__name__)
 class VfbHeaderParser:
     stream: BufferedReader | None = None
 
-    @classmethod
-    def parse(cls, stream: BufferedReader) -> Tuple[Dict[str, Any], int]:
-        cls.stream = stream
+    def parse(self, stream: BufferedReader) -> Tuple[Dict[str, Any], int]:
+        self.stream = stream
         header: Dict[str, Any] = {}
-        header["header0"] = cls.read_uint8()
-        header["filetype"] = cls.stream.read(5).decode("cp1252")
-        header["header1"] = cls.read_uint16()
-        header["header2"] = cls.read_uint16()
-        header["reserved"] = str(cls.stream.read(34))
-        header["header3"] = cls.read_uint16()
-        header["header4"] = cls.read_uint16()
-        header["header5"] = cls.read_uint16()
-        header["header6"] = cls.read_uint16()
-        header["header7"] = cls.read_uint16()
+        header["header0"] = self.read_uint8()
+        header["filetype"] = self.stream.read(5).decode("cp1252")
+        header["header1"] = self.read_uint16()
+        header["header2"] = self.read_uint16()
+        header["reserved"] = str(self.stream.read(34))
+        header["header3"] = self.read_uint16()
+        header["header4"] = self.read_uint16()
+        header["header5"] = self.read_uint16()
+        header["header6"] = self.read_uint16()
+        header["header7"] = self.read_uint16()
         if header["header7"] == 10:
             # FL5 additions over FL3
-            header["header8"] = cls.read_uint16()
+            header["header8"] = self.read_uint16()
             for i in range(9, 12):
-                key = cls.read_uint8()
+                key = self.read_uint8()
                 val = read_encoded_value(stream)
                 header[f"header{i}"] = {key: val}
-            header["header12"] = cls.read_uint8()
-            header["header13"] = cls.read_uint16()
+            header["header12"] = self.read_uint8()
+            header["header13"] = self.read_uint16()
         else:
             header["header13"] = header["header7"]
             del header["header7"]
-        header["header14"] = cls.read_uint16()
+        header["header14"] = self.read_uint16()
 
         # Get the size of the original binary data
-        datasize = cls.stream.tell()
+        datasize = self.stream.tell()
 
         return header, datasize
 
-    @classmethod
-    def read_uint8(cls) -> int:
-        assert cls.stream is not None
-        return int.from_bytes(cls.stream.read(uint8), byteorder="little", signed=False)
+    def read_uint8(self) -> int:
+        assert self.stream is not None
+        return int.from_bytes(self.stream.read(uint8), byteorder="little", signed=False)
 
-    @classmethod
-    def read_uint16(cls) -> int:
-        assert cls.stream is not None
-        return int.from_bytes(cls.stream.read(uint16), byteorder="little", signed=False)
+    def read_uint16(self) -> int:
+        assert self.stream is not None
+        return int.from_bytes(
+            self.stream.read(uint16), byteorder="little", signed=False
+        )
