8ba16749d3a7f350a8437b49550d2c80095fd362
Jens Kutilek <webmail@kutilek.de>
2023-12-01 13:50:09 +0100

    Fix decoding of mac strings in name table records in Python < 3.11

--- Lib/vfbLib/parsers/text.py
+++ Lib/vfbLib/parsers/text.py
@@ -27,7 +27,8 @@ class NameRecordsParser(BaseParser):
                     char = chr(c)
                     # Fix platform-specific encodings for Mac
                     if platID == 1 and encID == 0:
-                        char = c.to_bytes().decode("macroman")
+                        # TODO: Default arguments can be removed when Python < 3.11 is dropped
+                        char = c.to_bytes(length=1, byteorder="big").decode("macroman")
                 except ValueError:
                     char = "\ufeff"
                 name += char
