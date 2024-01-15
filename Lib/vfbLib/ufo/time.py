4e261dd15d5908be168abed43f3cfda0b25ab696
Jens Kutilek <webmail@kutilek.de>
2024-01-15 11:48:54 +0100

    Support Python < 3.11

--- Lib/vfbLib/ufo/time.py
+++ Lib/vfbLib/ufo/time.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
 
 from datetime import datetime
-from datetime import UTC
+from sys import version_info
+
+if version_info[0] == 3 and version_info[1] > 10:
+    from datetime import UTC
+else:
+    from datetime import timezone
+
+    UTC = timezone.utc
 
 
 TIME_ZERO = 2082841200
@@ -15,4 +22,6 @@ def convert_timestamp(t: int) -> datetime:
     if t < TIME_ZERO:
         t = TIME_ZERO
 
-    return datetime.fromtimestamp(t - TIME_ZERO, UTC)
+    if version_info[0] == 3 and version_info[1] > 10:
+        return datetime.fromtimestamp(t - TIME_ZERO, UTC)
+    return datetime.utcfromtimestamp(t - TIME_ZERO)
