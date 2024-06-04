18536b5e91b059c8508d045a13af756452b2e1e1
Jens Kutilek <webmail@kutilek.de>
2024-06-04 12:34:54 +0200

    Use the TrueType stem counts

--- Lib/vfbLib/parsers/truetype.py
+++ Lib/vfbLib/parsers/truetype.py
@@ -210,14 +210,15 @@ class TrueTypeStemPpemsParser(BaseParser):
 class TrueTypeStemPpems1Parser(BaseParser):
     # PPEM 1 for each stem is stored in a separate entry ...
     def _parse(self):
-        # FIXME
-        # The number of stems is not stored here, we must take it from elsewhere.
         stream = self.stream
         names = ("ttStemsV", "ttStemsH")
         result = {}
         for i in range(2):
             direction = []
-            num_stems = read_encoded_value(stream)
+            num_stems = (self.ttStemsV_count, self.ttStemsH_count)[i]
+            if num_stems is None:
+                raise ValueError
+
             for j in range(num_stems):
                 ppm = read_encoded_value(stream)
                 direction.append(
