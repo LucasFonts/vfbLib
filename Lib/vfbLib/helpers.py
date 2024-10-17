f2470c3a1a2e136c4582bf9281f659f2f189fdeb
Jens Kutilek <webmail@kutilek.de>
2024-10-17 11:03:13 +0200

    Add int type lengths to helpers

--- Lib/vfbLib/helpers.py
+++ Lib/vfbLib/helpers.py
@@ -1,3 +1,9 @@
+# Length of data types used by StreamReader and Stream Writer etc.
+uint8 = 1
+uint16 = 2
+uint32 = 4
+
+
 def binaryToIntList(value: int, start: int = 0):
     intList = []
     counter = start
