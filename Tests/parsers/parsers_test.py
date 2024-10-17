2f42b782ba19a3d37a116b31e24a9cc299e7347f
Jens Kutilek <webmail@kutilek.de>
2024-10-17 13:05:21 +0200

    Better format

--- Tests/parsers/parsers_test.py
+++ Tests/parsers/parsers_test.py
@@ -16,38 +16,46 @@ class OpenTypeClassFlagsParserTest(TestCase):
     def test_2_masters_kerning(self):
         expect(
             OpenTypeKerningClassFlagsParser,
+            # fmt: off
             (
-                "8e"  # 3 entries follow
+                "8e"                # 3 entries follow
+
                 # entry 1
-                "92"  # 7 = string length
-                "5f615f4c454654"  # _a_LEFT
-                "fa94"  # 1024
-                "8b"  # 0
+                "92"                # 7 = string length
+                "5f615f4c454654"    # _a_LEFT
+                "fa94"              # 1024
+                "8b"                # 0
+
                 # entry 2
-                "93"  # 8 = string length
+                "93"                # 8 = string length
                 "5f615f5249474854"  # _a_RIGHT
-                "ff00000800"  # 2048
-                "8b"  # 0
+                "ff00000800"        # 2048
+                "8b"                # 0
+
                 # entry 3
-                "8d"  # 2 = string length
-                "5f74"  # _t
-                "ff00000c00"  # 3072
-                "8b"  # 0
+                "8d"                # 2 = string length
+                "5f74"              # _t
+                "ff00000c00"        # 3072
+                "8b"                # 0
             ),
+            # fmt: on
             {"_a_LEFT": (1024, 0), "_a_RIGHT": (2048, 0), "_t": (3072, 0)},
         )
 
     def test_2_masters_metrics(self):
         expect(
             OpenTypeMetricsClassFlagsParser,
+            # fmt: off
             (
-                "8c"  # 1 entry follows
+                "8c"            # 1 entry follows
+
                 # entry 1
-                "91"  # 6 = string length
+                "91"            # 6 = string length
                 "2e6d74727835"  # .mtrx5
-                "8b"  # 0
-                "ff00001401"  # 5121
-                "8b"  # 0
+                "8b"            # 0
+                "ff00001401"    # 5121
+                "8b"            # 0
             ),
+            # fmt: on
             {".mtrx5": (0, 5121, 0)},
         )
