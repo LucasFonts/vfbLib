89515ba1e4ca270fa369f07d6d5c2f78d81a620c
Jens Kutilek <webmail@kutilek.de>
2024-11-08 16:56:24 +0100

    Remove testhelpers

--- Lib/vfbLib/testhelpers.py
+++
@@ -1,7 +0,0 @@
-from fontTools.misc.textTools import deHexStr
-from io import BytesIO
-
-
-def expect(parser, encoded, decoded):
-    data = deHexStr(encoded)
-    assert parser().parse(BytesIO(data), len(data)) == decoded
