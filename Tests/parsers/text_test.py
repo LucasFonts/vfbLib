5e5ce2daceffadb81e36e2ddb808fc1cc107751c
Jens Kutilek <webmail@kutilek.de>
2025-01-10 16:57:39 +0100

    Fix: strip null bytes and spaces from strings again

--- Tests/parsers/text_test.py
+++ Tests/parsers/text_test.py
@@ -9,6 +9,6 @@ from vfbLib.parsers.text import (
 class StringParserTest(TestCase):
     def test_links_1(self):
         data = "57 74 30 20 57 64 31 20"
-        expected = "Wt0 Wd1 "
+        expected = "Wt0 Wd1"  # Trailing space gets stripped
         result = StringParser().parse_hex(data)
         assert result == expected
