0ddb2a22819877a69f952da9880c905ef241d5f0
Jens Kutilek <webmail@kutilek.de>
2025-04-14 12:25:40 +0200

    Use size constants

--- src/vfbLib/parsers/value.py
+++ src/vfbLib/parsers/value.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+from vfbLib.helpers import int8_size, int32_size
 
 if TYPE_CHECKING:
     from io import BufferedReader, BytesIO
@@ -28,7 +29,7 @@ def read_value(stream: BufferedReader | BytesIO, signed=True) -> int:
     Returns:
         int: The decoded value.
     """
-    val = int.from_bytes(stream.read(1), byteorder="little")
+    val = int.from_bytes(stream.read(int8_size), byteorder="little")
     if val == 0:
         raise EOFError
 
@@ -41,20 +42,22 @@ def read_value(stream: BufferedReader | BytesIO, signed=True) -> int:
 
     elif val <= 0xFA:
         # 108 to 1131, represented by 2 bytes
-        val2 = int.from_bytes(stream.read(1), byteorder="little")
+        val2 = int.from_bytes(stream.read(int8_size), byteorder="little")
         # val - 0x8B + (val - 0xF7) * 0xFF + val2
         return 0x100 * val - 0xF694 + val2
 
     elif val <= 0xFE:
         # -108 to -1131, represented by 2 bytes
-        val2 = int.from_bytes(stream.read(1), byteorder="little")
+        val2 = int.from_bytes(stream.read(int8_size), byteorder="little")
         # 0x8F - val - (val - 0xFB) * 0xFF - val2
         return -0x100 * val + 0xFA94 - val2
 
     elif val == 0xFF:
         # 32 bit integer follows
         # FIXME: The Type1 spec says that it is always a signed int.
-        decoded = int.from_bytes(stream.read(4), byteorder="big", signed=signed)
+        decoded = int.from_bytes(
+            stream.read(int32_size), byteorder="big", signed=signed
+        )
         return decoded
 
     raise ValueError
