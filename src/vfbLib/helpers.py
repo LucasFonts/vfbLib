593fb05f9eeb4831349012d21d4c65ffffcde8fc
Jens Kutilek <webmail@kutilek.de>
2025-07-17 19:47:28 +0200

    Add intListToBinary helper

--- src/vfbLib/helpers.py
+++ src/vfbLib/helpers.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 # Length of data types used by StreamReader and Stream Writer etc.
 int8_size = 1
 int16_size = 2
@@ -14,6 +16,13 @@ def binaryToIntList(value: int, start: int = 0) -> list[int]:
         value >>= 1
         counter += 1
     return intList
+
+
+def intListToBinary(bits: Iterable[int]) -> int:
+    value = 0
+    for bit in bits:
+        value += 2**bit
+    return value
 
 
 def deHexStr(hexstr: str) -> bytes:
