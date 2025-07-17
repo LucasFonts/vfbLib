b511d84e581f670a7ee3f955263d4301c76bd98b
Jens Kutilek <webmail@kutilek.de>
2025-07-17 21:41:56 +0200

    Add test for binaryToIntList

---
+++ Tests/helpers_test.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+
+from vfbLib.helpers import binaryToIntList, deHexStr, hexStr, intListToBinary
+
+
+class HelpersTest(TestCase):
+    def test_binaryToIntList0(self):
+        assert binaryToIntList(0) == []
+
+    def test_binaryToIntList1(self):
+        assert binaryToIntList(1) == [0]
+
+    def test_binaryToIntList2(self):
+        assert binaryToIntList(2) == [1]
+
+    def test_binaryToIntList3(self):
+        assert binaryToIntList(3) == [0, 1]
+
+    def test_intListToBinary0(self):
+        assert intListToBinary([]) == 0
+
+    def test_intListToBinary1(self):
+        assert intListToBinary([0]) == 1
+
+    def test_intListToBinary2(self):
+        assert intListToBinary([1]) == 2
+
+    def test_intListToBinary3(self):
+        assert intListToBinary([0, 1]) == 3
