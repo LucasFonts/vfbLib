2bbca7417f7891198c83ae954b0120a8e949070b
Jens Kutilek <webmail@kutilek.de>
2025-07-22 10:58:39 +0200

    Implement GuidesCompiler

--- Tests/guides_test.py
+++ Tests/guides_test.py
@@ -75,3 +75,121 @@ class GuidePropertiesParserTest(TestCase):
     def test_partial(self):
         result = GuidePropertiesParser().parse_hex(bin_partial)
         assert result == raw_partial
+
+
+bin_guides = (
+    "9e"  # 19 horizontal guides
+    "fa688b"  # 980 0 (master 0)
+    "fa688b"  # 980 0 (master 1)
+    "fbd48b"  # -320 0 (master 0)
+    "fbd48b"  # -320 0 (master 1)
+    "f7be8b"
+    "f7be8b"
+    "f9ca8b"
+    "f9cb8b"
+    "f9c98b"
+    "f9c38b"
+    "f9378b"
+    "f9378b"
+    "f93f8b"
+    "f9418b"
+    "f8ba8b"
+    "f8b78b"
+    "f7f48b"
+    "f7ec8b"
+    "f7ec8b"
+    "f7e28b"
+    "f7528b"
+    "f7528b"
+    "f74d8b"
+    "f74b8b"
+    "388b"
+    "388b"
+    "fb238b"
+    "fb108b"
+    "fb2a8b"
+    "fb168b"
+    "fb348b"
+    "fb1d8b"
+    "f7c78b"
+    "f7c78b"
+    "f9b48b"
+    "f9b68b"
+    "f9ac8b"
+    "f9ac8b"
+    "8c"  # 1 vertical guide
+    "fbb68b"  # -290 0 (master 0)
+    "fbae8b"  # -282 0 (master 1)
+)
+
+raw_guides = {
+    "h": [
+        [
+            {"pos": 980, "angle": 0.0},
+            {"pos": -320, "angle": 0.0},
+            {"pos": 298, "angle": 0.0},
+            {"pos": 822, "angle": 0.0},
+            {"pos": 821, "angle": 0.0},
+            {"pos": 675, "angle": 0.0},
+            {"pos": 683, "angle": 0.0},
+            {"pos": 550, "angle": 0.0},
+            {"pos": 352, "angle": 0.0},
+            {"pos": 344, "angle": 0.0},
+            {"pos": 190, "angle": 0.0},
+            {"pos": 185, "angle": 0.0},
+            {"pos": -83, "angle": 0.0},
+            {"pos": -143, "angle": 0.0},
+            {"pos": -150, "angle": 0.0},
+            {"pos": -160, "angle": 0.0},
+            {"pos": 307, "angle": 0.0},
+            {"pos": 800, "angle": 0.0},
+            {"pos": 792, "angle": 0.0},
+        ],
+        [
+            {"pos": 980, "angle": 0.0},
+            {"pos": -320, "angle": 0.0},
+            {"pos": 298, "angle": 0.0},
+            {"pos": 823, "angle": 0.0},
+            {"pos": 815, "angle": 0.0},
+            {"pos": 675, "angle": 0.0},
+            {"pos": 685, "angle": 0.0},
+            {"pos": 547, "angle": 0.0},
+            {"pos": 344, "angle": 0.0},
+            {"pos": 334, "angle": 0.0},
+            {"pos": 190, "angle": 0.0},
+            {"pos": 183, "angle": 0.0},
+            {"pos": -83, "angle": 0.0},
+            {"pos": -124, "angle": 0.0},
+            {"pos": -130, "angle": 0.0},
+            {"pos": -137, "angle": 0.0},
+            {"pos": 307, "angle": 0.0},
+            {"pos": 802, "angle": 0.0},
+            {"pos": 792, "angle": 0.0},
+        ],
+    ],
+    "v": [[{"pos": -290, "angle": 0.0}], [{"pos": -282, "angle": 0.0}]],
+}
+
+bin_guides_empty = "8b8b"
+
+raw_guides_empty = {"h": [[], []], "v": [[], []]}
+
+
+class GlobalGuidesParserTest(TestCase):
+    def test(self):
+        result = GlobalGuidesParser().parse_hex(bin_guides, master_count=2)
+        assert result == raw_guides
+
+    def test_empty(self):
+        result = GlobalGuidesParser().parse_hex(bin_guides_empty, master_count=2)
+        assert result == raw_guides_empty
+
+
+class GuidesCompilerTest(TestCase):
+    def test(self):
+        result = GuidesCompiler().compile_hex(raw_guides, master_count=2)
+        assert result == bin_guides
+
+    def test_empty(self):
+        result = GuidesCompiler().compile_hex(raw_guides_empty, master_count=2)
+        assert result == bin_guides_empty
--- src/vfbLib/compilers/guides.py
+++ src/vfbLib/compilers/guides.py
@@ -1,17 +1,31 @@
 from __future__ import annotations
 
+from math import radians, tan
 from typing import TYPE_CHECKING
 
 from vfbLib import DIRECTIONS
 from vfbLib.compilers.base import BaseCompiler
 
 if TYPE_CHECKING:
-    from vfbLib.typing import GuideDict, GuidePropertiesDict
+    from vfbLib.typing import GuidePropertiesDict, MMGuidesDict
 
 
 class GuidesCompiler(BaseCompiler):
-    def _compile(self, data: GuideDict) -> None:
-        pass
+    def _compile(self, data: MMGuidesDict) -> None:
+        for direction in DIRECTIONS:
+            dir_guides = data[direction]
+            # if not dir_guides:
+            #     self.write_value(0)
+            #     continue
+
+            num_guides = len(dir_guides[0])
+            self.write_value(num_guides)
+            for guide_index in range(num_guides):
+                for master_index in range(self.master_count):
+                    guide = dir_guides[master_index][guide_index]
+                    self.write_value(guide["pos"])
+                    angle = int(tan(radians(guide["angle"])) * 10000)
+                    self.write_value(angle)
 
 
 class GuidePropertiesCompiler(BaseCompiler):
