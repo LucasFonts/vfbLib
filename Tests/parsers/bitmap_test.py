0b63dba6c06f07cc9a0a9b1f06b25b4488a3c966
Jens Kutilek <webmail@kutilek.de>
2025-07-28 15:03:34 +0200

    Remove import from test

--- Tests/parsers/bitmap_test.py
+++ Tests/parsers/bitmap_test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from vfbLib.parsers.bitmap import BackgroundBitmapParser, pprint_bitmap
+from vfbLib.parsers.bitmap import BackgroundBitmapParser
 
 bitmaps = [
     {
