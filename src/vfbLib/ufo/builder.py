aed92a0655e619ceaa7c7b4eb5d91bab0d8074d2
Jens Kutilek <webmail@kutilek.de>
2026-01-16 09:29:31 +0100

    Typing

--- src/vfbLib/ufo/builder.py
+++ src/vfbLib/ufo/builder.py
@@ -89,7 +89,7 @@ class VfbToUfoBuilder:
         self.groups: UfoGroups = {}
         self.guide_properties = GuidePropertiesDict(h=[], v=[])
         self.info = VfbToUfoInfo()
-        self.kerning_class_flags = KerningClassFlagDict()
+        self.kerning_class_flags: KerningClassFlagDict = KerningClassFlagDict()
         self.num_blue_values = 0
         self.num_other_blues = 0
         self.num_family_blues = 0
